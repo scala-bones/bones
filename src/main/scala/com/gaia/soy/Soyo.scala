71b4eb5f70fe93cb40b874c9b3d1bdf77727a2ef
Travis Stevens <Travis.Stevens@gaiam.com>
2017-10-29 23:03:26 -0400

    Some example tests running with group

--- src/main/scala/com/gaia/soy/Soyo.scala
+++ src/main/scala/com/gaia/soy/Soyo.scala
@@ -1,39 +1,73 @@
 package com.gaia.soy
 
-import cats.{Apply, Id}
-import cats.data.NonEmptyList
+import cats.{Applicative, Apply, Id}
+import cats.data.{NonEmptyList, Validated}
 import cats.implicits._
-
+import cats.free.FreeApplicative.lift
+import com.gaia.soy.StringValidation.{OptionalString, RequiredString}
 
 object Soyo {
 
+  type FG[A] = FieldGroup[ValidationResultNel[A]]
+  type FGO[A] = FieldGroupOp[ValidationResultNel[A]]
 
-  case class Ap2[A,B,F[_],G[_]](e1: Extraction[F,A], e2: Extraction[G,B]) extends Extraction[Id,(F[A],G[B])] {
-    override type I = String
+//  case class Obj4[A,B,C,D,E](op1: FGO[A], op2: FGO[B], op3: FGO[C], op4: FGO[D]) extends FGO[(A,B,C,D)] {
+//
+//    def apply(f: (A,B,C,D) => E): FG[E] = {
+//      (
+//        lift(op1).asInstanceOf[FieldGroup[ValidationResultNel[A]]],
+//        lift(op2).asInstanceOf[FieldGroup[ValidationResultNel[B]]],
+//        lift(op3).asInstanceOf[FieldGroup[ValidationResultNel[C]]],
+//        lift(op4).asInstanceOf[FieldGroup[ValidationResultNel[D]]]
+//      ).mapN { case (v1,v2,v3,v4) =>  {
+//        Applicative[Validated[NonEmptyList[ExtractionError], ?]].map4(v1,v2,v3,v4)(f(_, _, _, _))
+//      }}
+//    }
+//  }
 
-    override def extract(stringProducer: RawValueProducer): Either[ExtractionErrors, (F[A], G[B])] = {
 
-      val r1 = e1.extract(stringProducer)
-      val r2 = e2.extract(stringProducer)
-      Apply[Either[ExtractionErrors,?]].map2(r1,r2)( (a,b) => (a,b))
-
-    }
+  def obj4[A,B,C,D,E](op1: FGO[A],
+                  op2: FGO[B],
+                  op3: FGO[C],
+                  op4: FGO[D],
+                  f: (A,B,C,D) => E) : FG[E] = {
+    (
+      lift(op1).asInstanceOf[FieldGroup[ValidationResultNel[A]]],
+      lift(op2).asInstanceOf[FieldGroup[ValidationResultNel[B]]],
+      lift(op3).asInstanceOf[FieldGroup[ValidationResultNel[C]]],
+      lift(op4).asInstanceOf[FieldGroup[ValidationResultNel[D]]]
+    ).mapN { case (v1,v2,v3,v4) =>  {
+      Applicative[Validated[NonEmptyList[ExtractionError], ?]].map4(v1,v2,v3,v4)(f(_, _, _, _))
+    }}
   }
 
 
-  case class Ap4[A,B,C,D,FA[_],FB[_],FC[_],FD[_]](
-    e1: Extraction[FA,A], e2: Extraction[FB,B], e3: Extraction[FC,C], e4: Extraction[FD,D]
-  ) extends Extraction[Id,(FA[A], FB[B], FC[C], FD[D])] {
-    override type I = this.type
-
-    override def extract(stringProducer: RawValueProducer): Either[ExtractionErrors, (FA[A], FB[B], FC[C], FD[D])] = {
-      val r1 = e1.extract(stringProducer)
-      val r2 = e2.extract(stringProducer)
-      val r3 = e3.extract(stringProducer)
-      val r4 = e4.extract(stringProducer)
-      Apply[Either[ExtractionErrors, ?]].map4(r1, r2, r3, r4)((a, b, c, d) => (a, b, c, d))
-    }
-  }
+//  case class Ap2[A,B,F[_],G[_]](e1: Extraction[F,A], e2: Extraction[G,B]) extends Extraction[Id,(F[A],G[B])] {
+//    override type I = String
+//
+//    override def extract(stringProducer: StringProducer): Either[ExtractionErrors, (F[A], G[B])] = {
+//
+//      val r1 = e1.extract(stringProducer)
+//      val r2 = e2.extract(stringProducer)
+//      Apply[Either[ExtractionErrors,?]].map2(r1,r2)( (a,b) => (a,b))
+//
+//    }
+//  }
+//
+//
+//  case class Ap4[A,B,C,D,FA[_],FB[_],FC[_],FD[_]](
+//    e1: Extraction[FA,A], e2: Extraction[FB,B], e3: Extraction[FC,C], e4: Extraction[FD,D]
+//  ) extends Extraction[Id,(FA[A], FB[B], FC[C], FD[D])] {
+//    override type I = this.type
+//
+//    override def extract(stringProducer: RawValueProducer): Either[ExtractionErrors, (FA[A], FB[B], FC[C], FD[D])] = {
+//      val r1 = e1.extract(stringProducer)
+//      val r2 = e2.extract(stringProducer)
+//      val r3 = e3.extract(stringProducer)
+//      val r4 = e4.extract(stringProducer)
+//      Apply[Either[ExtractionErrors, ?]].map4(r1, r2, r3, r4)((a, b, c, d) => (a, b, c, d))
+//    }
+//  }
 
 }
 
--- src/main/scala/com/gaia/soy/StringValidation.scala
+++ src/main/scala/com/gaia/soy/StringValidation.scala
@@ -13,58 +13,132 @@ import scala.util.matching.Regex
 object StringValidation {
 
   private val alphanumRegx = "^[a-zA-Z0-9]*$".r
+
   case class IsAlphanum() extends ValidationOp[String] {
     val isValid: String => Boolean = alphanumRegx.findFirstMatchIn(_).isDefined
+
+    override def defaultError(t: String): String = s"${t} is not alphanumeric"
+
+    override def description: String = s"alphanumeric"
   }
+
   case class Min(min: Int) extends ValidationOp[String] {
     val isValid: String => Boolean = _.length >= min
+
+    override def defaultError(t: String): String = s"${t} is less than ${min}"
+
+    override def description: String = s"minimum of ${min}"
   }
+
   case class Max(max: Int) extends ValidationOp[String] {
     val isValid: String => Boolean = _.length <= max
+
+    override def defaultError(t: String): String = s"${t} is greater than ${max}"
+
+    override def description: String = s"maximum of ${max}"
   }
+
   case class MatchesRegex(r: Regex) extends ValidationOp[String] {
     val isValid: String => Boolean = r.findFirstMatchIn(_).isDefined
+
+    override def defaultError(t: String): String = s"${t} does not match regular expression ${r.toString}"
+
+    override def description: String = s"must match regular expression ${r.toString}"
   }
+
   case class Length(length: Int) extends ValidationOp[String] {
     override def isValid: (String) => Boolean = _.length == length
+
+    override def defaultError(t: String): String = s"${t} does not have length ${length}"
+
+    override def description: String = s"length of ${length}"
   }
-  case class Custom(f: String => Boolean) extends ValidationOp[String] {
+
+  case class Custom(f: String => Boolean, defaultErrorF: String => String, description: String) extends ValidationOp[String] {
     val isValid: String => Boolean = f
+
+    override def defaultError(t: String): String = defaultErrorF(t)
   }
+
   case class Guid() extends ValidationOp[String] {
-    val isValid: String => Boolean = str => Try { UUID.fromString(str) }.toEither.fold(_ => false, _ => true)
+    val isValid: String => Boolean = str => Try {
+      UUID.fromString(str)
+    }.toEither.fold(_ => false, _ => true)
+
+    override def defaultError(t: String): String = s"${t} is not a GUID"
+
+    override def description: String = "be a GUID"
   }
+
   case class Uppercase() extends ValidationOp[String] {
     val isValid: String => Boolean = str => str.toUpperCase === str
+
+    override def defaultError(t: String): String = s"${t} must be uppercase"
+
+    override def description: String = "uppercase"
   }
+
   case class CreditCard() extends ValidationOp[String] {
     override def isValid: (String) => Boolean = ??? //luhn check
+    override def defaultError(t: String): String = s"${t} is not a valid credit card number"
+
+    override def description: String = "valid credit card number"
   }
+
   private val tokenRegex = "^[a-zA-Z0-9_]*$".r
+
   case class Token() extends ValidationOp[String] {
     override def isValid: (String) => Boolean = tokenRegex.findFirstMatchIn(_).isDefined
+
+    override def defaultError(t: String): String = s"${t} is not a token"
+
+    override def description: String = "token"
   }
+
   case class Email() extends ValidationOp[String] {
     override def isValid: (String) => Boolean = ???
+
+    override def defaultError(t: String): String = s"${t} is not a valid email"
+
+    override def description: String = "email"
   }
+
   case class Hex() extends ValidationOp[String] {
     override def isValid: (String) => Boolean = ???
+
+    override def defaultError(t: String): String = s"${t} is not hexadecimal"
+
+    override def description: String = "hexadecimal"
   }
+
   case class Base64() extends ValidationOp[String] {
     override def isValid: (String) => Boolean = ???
+
+    override def defaultError(t: String): String = s"${t} is not Base64"
+
+    override def description: String = "Base64"
   }
+
   case class Hostname() extends ValidationOp[String] {
     override def isValid: (String) => Boolean = ??? //RFC1123 hostname
+    override def defaultError(t: String): String = s"${t} is not a hostname"
+
+    override def description: String = "RFC1123 hostname"
   }
+
   case class Lowercase() extends ValidationOp[String] {
     override def isValid: (String) => Boolean = str => str.toLowerCase === str
+
+    override def defaultError(t: String): String = s"${t} is not all lowercase"
+
+    override def description: String = "lowercase"
   }
 
   case class Ip() //enum ip. ipv4, ipv6, cidr
   case class Uri() //
 
 
-  def runValidation(key: Key, input: String, stringValidation: ValidationOp[String]): Validated[ValidationError[String], String] = {
+  private def runValidation(key: Key, input: String, stringValidation: ValidationOp[String]): Validated[ValidationError[String], String] = {
     if (stringValidation.isValid(input)) {
       Valid(input)
     } else {
@@ -74,152 +148,101 @@ object StringValidation {
 
 
   /**
-    * This is for the syntactic sure of adding validation types.
+    * This is for the syntactic sugar of adding validation types.
+    *
     * @tparam NSE The 'Next StringExtraction' type if a new value is appended.
     */
-  trait StringExtraction[NSE <: Extraction[F,String], F[_]] extends ExtractionAppend[String, F, NSE] {
+  trait StringExtraction[F[_], NSE] extends ExtractionAppend[String, F, NSE] {
 
     def length(size: Int): NSE = append(Length(size))
+
     def min(min: Int): NSE = append(Min(min))
+
     def max(max: Int): NSE = append(Max(max))
+
     def matchesRegex(r: Regex): NSE = append(MatchesRegex(r))
+
     def alphanum(): NSE = append(IsAlphanum())
+
     def guid(): NSE = append(Guid())
+
     def email(): NSE = append(Email())
+
     def token(): NSE = append(Token())
+
     def hex(): NSE = append(Hex())
+
     def base64(): NSE = append(Base64())
+
     def replace(): NSE = ??? //convert ???
     def hostname(): NSE = ???
+
     def normalize(): NSE = ??? //convert ???
     def lowercase(): NSE = append(Lowercase())
 
 
   }
 
-  /** This is the default extraction from calling Key.string().  It is an optional string
-    * without any validation so compiling simply returns the produced value.
-    * The only data error that can occur is if the value from the producer is not a string type.
-    * @param key The extraction key.
-    */
-  final case class OptionalStringOnly(key: Key) extends Extraction[Option,String] with StringExtraction[OptionalStringWithValidation,Option]
-     { extraction =>
-
-    type I = String
-
-    def append(sv: ValidationOp[String]) : OptionalStringWithValidation =
-      OptionalStringWithValidation(key, NonEmptyList.one(sv))
-
-//    def convert[O](c: Conversion[Option,String,O]) = OptionalStringOnlyConversion(extraction,c)
-
-    override def extract(stringProducer: RawValueProducer): Either[ExtractionErrors, Option[String]] =
-      stringProducer.produce[String](key).left.map(NonEmptyList.one)
-
-  }
-
-  /** When we want to convert from a String to another data type. */
-//  final case class OptionalStringOnlyConversion[O](optionalStringOnly: OptionalStringOnly, c: Conversion[Option,String,O])
-//    extends Extraction[Option,O]{
-//    type I = String
-//
-//    override def extract(input: RawValueProducer): Either[ExtractionErrors, Option[O]] =
-//      optionalStringOnly.extract(input).flatMap(e => e match {
-//        case Some(str) => c.convert(str)
-//          .map(o => Some(o))
-//          .left.map(_ => NonEmptyList.one(ValidationError(optionalStringOnly.key, c, Some(str))))
-//        case None => Right(None)
-//      }
-//    )
-//
-//  }
-
-  /** This is an extraction without validation, but a value is required from the producer.
-    * It will return an error if the producer returns a none for the key type as well
-    * as if the value from the producer is not a string type.
-    **/
-  final case class RequiredStringOnly(key: Key) extends Extraction[Id,String] with StringExtraction[RequiredStringWithValidation,Id] {
-
-    type I = String
-
-    def optional() = OptionalStringOnly(key)
-
-    def extract(input: RawValueProducer): Either[ExtractionErrors,String] =
-      input.produce[String](key).left.map(NonEmptyList.one).flatMap(res => res match {
-        case Some(e) => Right(e)
-        case None=> Left(NonEmptyList.one(ValidationError(key, RequiredOp(), res)))
+  private def runAndMapValidations(key: Key, str: String, validations: List[ValidationOp[String]]): ValidationResultNel[String] =
+    validations.map(runValidation(key, str, _))
+      .foldLeft[ValidatedNel[ValidationError[String], String]](Valid(str))((last, next) => {
+        val n: ValidatedNel[ValidationError[String], String] = next.leftMap(NonEmptyList.one)
+        Apply[ValidatedNel[ValidationError[String], ?]].map2(last, n)((a, b) => b)
       })
 
-    def append(sv: ValidationOp[String]) : RequiredStringWithValidation = RequiredStringWithValidation(key, NonEmptyList.one(sv))
 
-  }
+  final case class OptionalString(key: Key, validations: List[ValidationOp[String]]) extends FieldGroupOp[Validated[NonEmptyList[ExtractionError], Option[String]]]
+    with StringExtraction[Option, OptionalString] {
 
-//  final case class RequiredStringOnlyConversion[O](requiredStringOnly: RequiredStringOnly, c: ConversionOp[String,O])
-//    extends Extraction[Id,O]{
-//
-//    override type I = String
-//    def extract(input: RawValueProducer): Either[ExtractionErrors, O] = {
-//      requiredStringOnly.extract(input).flatMap(res =>
-//        c.convert(res).left.map(_ => NonEmptyList.one(ValidationError(requiredStringOnly.key, c, Some(res))))
-//      )
-//    }
-//
-//  }
-
-  def runAndMapValidations(key: Key, str: String, validations: NonEmptyList[ValidationOp[String]]): ValidationResultNel[String] =
-    validations.map(runValidation(key, str, _))
-      .foldLeft[ValidatedNel[ValidationError[String], String]](Valid(str))( (last,next) => {
-        val n: ValidatedNel[ValidationError[String], String] = next.leftMap(NonEmptyList.one)
-        Apply[ValidatedNel[ValidationError[String], ?]]
-          .map2(last,n)( (a,b) => b)
-    })
-
-
-  final case class OptionalStringWithValidation(key: Key, validations: NonEmptyList[ValidationOp[String]]) extends Extraction[Option,String]
-    with StringExtraction[OptionalStringWithValidation,Option] {
-
-    def extract(input: RawValueProducer): Either[ExtractionErrors, Option[String]] = {
-      input.produce[String](key).toValidatedNel.andThen(oStr => oStr match {
+    def extract(input: StringProducer): Validated[ExtractionErrors, Option[String]] = {
+      input.produceString(key).toValidatedNel.andThen(oStr => oStr match {
         case Some(str) => {
           runAndMapValidations(key, str, validations).map(Some(_))
         }
         case None => Valid(None)
-      }).toEither
+      })
     }
 
-    def append(sv: ValidationOp[String]) : OptionalStringWithValidation = OptionalStringWithValidation(key, sv :: validations)
+
+    override def appendMetadata(md: Metadata): OptionalString = ???
+
+    def append(sv: ValidationOp[String]): OptionalString = OptionalString(key, sv :: validations)
   }
 
-  final case class RequiredStringWithValidation(key: Key, validations: NonEmptyList[ValidationOp[String]]) extends Extraction[Id,String]
-    with StringExtraction[RequiredStringWithValidation,Id]{
+  final case class RequiredString(key: Key, validations: List[ValidationOp[String]]) extends FieldGroupOp[Validated[NonEmptyList[ExtractionError], String]]
+    with StringExtraction[Id, RequiredString] {
 
-    def optional() : OptionalStringWithValidation = OptionalStringWithValidation(key, validations)
+    def optional(): OptionalString = OptionalString(key, validations)
 
-    def extract(input: RawValueProducer): Either[NonEmptyList[ExtractionError], String] = {
-      input.produce[String](key).toValidated.leftMap(NonEmptyList.one).andThen(res => res match {
+    def extract(producer: StringProducer): Validated[NonEmptyList[ExtractionError], String] = {
+      producer.produceString(key).toValidated.leftMap(NonEmptyList.one).andThen(res => res match {
         case Some(e) => Valid(e)
-        case None=> Invalid(NonEmptyList.one(ValidationError(key, RequiredOp(), res)))
+        case None => Invalid(NonEmptyList.one(ValidationError(key, RequiredOp(), res)))
       }).andThen(str =>
         runAndMapValidations(key, str, validations)
-      ).toEither
+      )
     }
-    def append(sv: ValidationOp[String]) : RequiredStringWithValidation = RequiredStringWithValidation(key, sv :: validations)
+
+    def append(sv: ValidationOp[String]): RequiredString = RequiredString(key, sv :: validations)
+
+    override def appendMetadata(md: Metadata): RequiredString = ???
   }
 
-  /******** Conversion *************/
+  /** ****** Conversion *************/
 
-//  trait StringConversion[O] {
-//    def conversionOp: ConversionOp[String,O]
-//  }
-//
-//  case class ToBigDecimal() extends ConversionOp[String, BigDecimal] {
-//    def convert(str: String) : Either[String, BigDecimal] =
-//      Try { BigDecimal(str) }.toEither.left.map(_ => "Could not convert String to BigDecimal")
-//  }
-//
-//  case class ToUuid() extends ConversionOp[String, UUID] {
-//    override def convert(iToO: String): Either[String, UUID] =
-//      Try { UUID.fromString(iToO) }.toEither.left.map(_ => "Could not convert String to UUID")
-//  }
+  //  trait StringConversion[O] {
+  //    def conversionOp: ConversionOp[String,O]
+  //  }
+  //
+  //  case class ToBigDecimal() extends ConversionOp[String, BigDecimal] {
+  //    def convert(str: String) : Either[String, BigDecimal] =
+  //      Try { BigDecimal(str) }.toEither.left.map(_ => "Could not convert String to BigDecimal")
+  //  }
+  //
+  //  case class ToUuid() extends ConversionOp[String, UUID] {
+  //    override def convert(iToO: String): Either[String, UUID] =
+  //      Try { UUID.fromString(iToO) }.toEither.left.map(_ => "Could not convert String to UUID")
+  //  }
 
 }
 
