e312d7d6028143911f0de2be7c58ac45aae9e112
Travis Stevens <travis01010011@gmail.com>
2019-01-27 09:31:39 -0500

    first run at protobuf

--- json-interpreters/protobuf/src/main/scala/com/bones/protobuf/ProtoFileInterpreter.scala
+++ json-interpreters/protobuf/src/main/scala/com/bones/protobuf/ProtoFileInterpreter.scala
@@ -1,5 +1,42 @@
 package com.bones.protobuf
 
+import com.bones.data.Value._
+import shapeless.{HList, Nat}
+
 object ProtoFileInterpreter {
+
+  def dataClass[A](dc: DataClass[A]): Unit = {
+    dc match {
+      case t: XMapData[a, al, b] => ???
+      case o: OptionalDataClass[a] => ???
+      case ld: XMapListData[b] => ???
+    }
+  }
+
+  def kvpGroup[H<:HList,HL<:Nat](group: KvpGroup[H,HL]): Unit = {
+    group match {
+      case KvpNil => ???
+      case op: KvpSingleValueHead[h, t, tl, a] => ???
+      case op: KvpGroupHead[a, al, h, hl, t, tl] => ???
+      case op: KvpDataClassHead[h,t,tl,out] => ???
+      case op: OptionalKvpGroup[h,hl] => ???
+    }
+  }
+
+  def valueDefinition[A](fgo: ValueDefinitionOp[A]): Unit =
+    fgo match {
+      case op: OptionalValueDefinition[a] => ???
+      case ob: BooleanData => ???
+      case rs: StringData => ???
+      case ri: LongData => ???
+      case uu: UuidData => ???
+      case dd: DateTimeData => ???
+      case bd: BigDecimalData => ???
+      case ld: ListData[t] => ???
+      case ed: EitherData[a,b] => ???
+      case esd: EnumerationStringData[a] => ???
+      case esd: EnumStringData[a] => ???
+      case kvp: KvpGroupData[h,hl] => ???
+    }
 
 }
--- project/plugin.sbt
+++ project/plugin.sbt
@@ -1,1 +1,2 @@
 // addSbtPlugin("org.tpolecat" % "tut-plugin" % "0.6.9")
+addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.26")
