0e68ba49d1e600185d0647075f1b304ab4ce7e37
Travis Stevens <travis01010011@gmail.com>
2019-04-09 00:33:19 -0400

    added DbInsert values interpreter

--- db-interpreters/doobie/src/main/scala/com/bones/doobie/DbColumnInterpreter.scala
+++ db-interpreters/doobie/src/main/scala/com/bones/doobie/DbColumnInterpreter.scala
@@ -1,63 +1,49 @@
 package com.bones.doobie
 
 import com.bones.data.Value._
-import shapeless._, ops.hlist.IsHCons
+import shapeless.{HNil,HList,Nat}
 
+/** Responsible for getting a list of columns for the select or insert clause */
 object DbColumnInterpreter {
 
   type ColumnName = String
-  type GroupToColumns[A] = A => List[String]
-  type ToColumns[A] = ColumnName => A => List[String]
+  type ToColumns = ColumnName => List[String]
 
 
-  def kvpGroup[H<:HList,HL<:Nat](group: KvpGroup[H,HL]): GroupToColumns[H] = {
+  private def kvpGroup[H<:HList,HL<:Nat](group: KvpGroup[H,HL]): List[String] = {
     group match {
-      case KvpNil => _ => List.empty
-      case op: KvpSingleValueHead[h, t, tl, a] => {
-        val headF = valueDefinition(op.fieldDefinition.op)(op.fieldDefinition.key)
-        val tailF = kvpGroup(op.tail)
-        (input: H) => {
-          val headResult = headF(input.head)
-          val tailResult = tailF(input.tail)
-          headResult ::: tailResult
-        }
-      }
+      case KvpNil => List.empty
+      case op: KvpSingleValueHead[h, t, tl, a] =>
+        val headResult = valueDefinition(op.fieldDefinition.op)(op.fieldDefinition.key)
+        val tailResult = kvpGroup(op.tail)
+        headResult ::: tailResult
       case op: KvpGroupHead[a, al, h, hl, t, tl] =>
-        val headF = kvpGroup(op.head)
-        val tailF = kvpGroup(op.tail)
-        (in: H) => {
-          val cast = in.asInstanceOf[h :: t]
-          headF(cast.head) ::: tailF(cast.tail)
-        }
+        val headResult = kvpGroup(op.head)
+        val tailResult = kvpGroup(op.tail)
+        headResult ::: tailResult
       case op: OptionalKvpGroup[h,hl] => ???
     }
   }
 
-  def valueDefinition[A](fgo: ValueDefinitionOp[A]): ToColumns[A] =
+  private val nameToColumn: ColumnName => List[String] = name => DoobieUtil.camelToSnake(name) :: Nil
+
+  private def valueDefinition[A](fgo: ValueDefinitionOp[A]): ToColumns =
     fgo match {
-      case op: OptionalValueDefinition[b] => {
-        val valueF = valueDefinition(op.valueDefinitionOp)
-        (name: ColumnName) => {
-          val trueName = valueF(name)
-          (a: Option[b]) => {
-            a match {
-              case None => List.empty
-              case Some(input) => trueName(input)
-            }
-          }
-        }
-      }
-      case ob: BooleanData => ???
-      case rs: StringData => ???
-      case ri: LongData => ???
-      case uu: UuidData => ???
-      case dd: DateTimeData => ???
-      case bd: BigDecimalData => ???
-      case ld: ListData[t] => ???
-      case ed: EitherData[a,b] => ???
-      case esd: EnumerationStringData[a] => ???
-      case esd: EnumStringData[a] => ???
-      case kvp: KvpGroupData[h,hl] => ???
-      case x: XMapData[a,al,b] => ???
+      case op: OptionalValueDefinition[b] =>
+          valueDefinition(op.valueDefinitionOp)
+      case ob: BooleanData => nameToColumn
+      case rs: StringData => nameToColumn
+      case ri: LongData => nameToColumn
+      case uu: UuidData => nameToColumn
+      case dd: DateTimeData => nameToColumn
+      case bd: BigDecimalData => nameToColumn
+      case ld: ListData[t] => nameToColumn
+      case ed: EitherData[a,b] => nameToColumn
+      case esd: EnumerationStringData[a] => nameToColumn
+      case esd: EnumStringData[a] => nameToColumn
+      case kvp: KvpGroupData[h,hl] =>
+        _ => kvpGroup(kvp.kvpGroup)
+      case x: XMapData[a,al,b] =>
+        _ => kvpGroup(x.from)
     }
 }
