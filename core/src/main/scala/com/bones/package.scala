e23ccc07dd8328562df748a1c60ba53975d535ec
Travis Stevens <travis01010011@gmail.com>
2019-11-30 11:43:44 -0500

    working towards coproducts

--- core/src/main/scala/com/bones/package.scala
+++ core/src/main/scala/com/bones/package.scala
@@ -46,4 +46,41 @@ package object bones {
 
   }
 
+  object covSyntax extends com.bones.coproduct.Sugar with com.bones.coproduct.KeyValueDefinitionSugar {
+
+    /** sv = String Validation */
+    val sv = StringValidation
+
+    /** lv = Long validation */
+    val lv = LongValidation
+
+    /** iv = Int validation */
+    val iv = IntValidation
+
+    /** bdv = Big Decimal Validation */
+    val bdv = BigDecimalValidation
+
+    /** fv = Float Validation */
+    val fv = FloatValidation
+
+    /** cv = Char Validation */
+    val cv = CharValidation
+
+    /** bv = Byte Validation */
+    val bv = ByteValidation
+
+    /** shv = Short Validation */
+    val shv = ShortValidation
+
+    /** dv = double validation */
+    val dv = DoubleValidation
+
+    /** ldtv = LocalDateTimeValidationInstances */
+    val ldtv = LocalDateTimeValidationInstances
+
+    /** ldv = LocalDateValidationInstances */
+    val ldv = LocalDateValidationInstances
+
+  }
+
 }
