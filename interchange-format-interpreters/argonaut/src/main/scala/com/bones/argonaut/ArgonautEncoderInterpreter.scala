6d40da918b681d613ba2d6185bfc6dd6c3e52ad9
Travis Stevens <travis.stevens@gaia.com>
2020-08-19 09:47:18 -0400

    refactored argonaut

--- interchange-format-interpreters/argonaut/src/main/scala/com/bones/argonaut/ArgonautEncoderInterpreter.scala
+++ interchange-format-interpreters/argonaut/src/main/scala/com/bones/argonaut/ArgonautEncoderInterpreter.scala
@@ -10,53 +10,20 @@ import com.bones.interpreter.KvpInterchangeFormatEncoderInterpreter
   * Module responsible for converting values to Argonaut JSON without validation.
   * The entry point for this class is [KvpInterchangeFormatEncoderInterpreter.encoderFromSchema].
   */
-trait ArgonautEncoderInterpreter extends KvpInterchangeFormatEncoderInterpreter[Json] {
+trait ArgonautEncoderInterpreter[ALG[_]] extends KvpInterchangeFormatEncoderInterpreter[ALG, Json] {
 
-  override def none: Json = Json.jNull
-
-  override def empty: Json = Json.obj()
-
-  override def combine(prefix: Json, postfix: Json): Json = {
-    val values1 = prefix.obj.toList.flatMap(_.toList)
-    val values2 = postfix.obj.toList.flatMap(_.toList)
+  override def combine(a: Json, b: Json): Json = {
+    val values1 = a.obj.toList.flatMap(_.toList)
+    val values2 = b.obj.toList.flatMap(_.toList)
     Json.obj(values1 ::: values2: _*)
   }
 
-  override def toObj[ALG[_], A](kvDef: KeyDefinition[ALG, A], value: Json): Json =
-    Json.obj((kvDef.key, value))
-
-  /** Create a function which converts a boolean into the specific OUT type */
-  override def booleanToOut: Boolean => Json =
-    input => Json.jBool(input)
-
-  /** Create a function which converts a String into the specific OUT type */
-  override def stringToOut: String => Json =
-    input => Json.jString(input)
-
-  override def intToOut: Int => Json = Json.jNumber
-
-  override def longToOut: Long => Json =
-    input => Json.jNumber(input)
-
-  override def floatToOut: Float => Json =
-    input => Json.jNumber(input.toDouble)
-
-  override def doubleToOut: Double => Json =
-    input => Json.jNumber(input)
-
-  override def bigDecimalToOut: BigDecimal => Json =
-    input => Json.jNumber(input)
-
-  override def byteArrayToOut: Array[Byte] => Json =
-    input => Json.jString(Base64.getEncoder.encodeToString(input))
-
-  override def toOutList(list: List[Json]): Json =
-    Json.array(list: _*)
-
-  override def shortToOut: Short => Json =
-    input => Json.jNumber(input.toInt)
+  override def empty: Json = Json.jEmptyObject
 
   override def addStringField(element: Json, name: String, value: String): Json =
     Json.obj((name, Json.jString(value)) :: element.obj.toList.flatMap(_.toList): _*)
+
+  override def toObj[A](kvDef: KeyDefinition[ALG, A], value: Json): Json =
+    Json.obj((kvDef.key, value))
 
 }
