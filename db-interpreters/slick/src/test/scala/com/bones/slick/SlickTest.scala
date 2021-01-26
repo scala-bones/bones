31d4e5cd366101fa22868bddc6e2a61ed19f9332
Travis Stevens <travis01010011@gmail.com>
2021-01-26 00:42:54 -0500

    another attempt at compile error fix

--- db-interpreters/slick/src/test/scala/com/bones/slick/SlickTest.scala
+++ db-interpreters/slick/src/test/scala/com/bones/slick/SlickTest.scala
@@ -13,46 +13,46 @@ import slickless._
 class SlickTest { // extends AnyFunSpec {
 
   // Definition of the SUPPLIERS table
-  class Suppliers(tag: Tag)
-      extends Table[(Int, String, String, String, String, String)](tag, "SUPPLIERS") {
-    def id = column[Int]("SUP_ID", O.PrimaryKey) // This is the primary key column
-    def name = column[String]("SUP_NAME")
-    def street = column[String]("STREET")
-    def city = column[String]("CITY")
-    def state = column[String]("STATE")
-    def zip = column[String]("ZIP")
-    // Every table needs a * projection with the same type as the table's type parameter
-    def * = (id, name, street, city, state, zip)
-  }
-  val suppliers = TableQuery[Suppliers]
-
-  // Definition of the COFFEES table
-  class Coffees(tag: Tag)
-      extends Table[String :: Int :: Double :: Int :: Int :: HNil](tag, "COFFEES") {
-    def name = column[String]("COF_NAME", O.PrimaryKey)
-    def supID = column[Int]("SUP_ID")
-    def price = column[Double]("PRICE")
-    def sales = column[Int]("SALES")
-    def total = column[Int]("TOTAL")
-    def * = name :: supID :: price :: sales :: total :: HNil
-    // A reified foreign key relation that can be navigated to create a join
-    def supplier = foreignKey("SUP_FK", supID, suppliers)(_.id)
-  }
-  val coffees = TableQuery[Coffees]
-
-  coffees.map(c => c.name)
-
-  case class KvpSlick[C](jsonName: String, slickColumn: Rep[C])
-
-  val coffeeSchema =
-    ("name", string(sv.unique)) ::
-      ("subID", int(iv.positive)) ::
-      ("price", double(dv.positive)) ::
-      ("sales", int(iv.positive)) ::
-      ("total", int(iv.positive)) ::
-      KvpNil[String, DefaultValues]
-
-  val coffeeSchemaTupled = coffeeSchema.tupled
+//  class Suppliers(tag: Tag)
+//      extends Table[(Int, String, String, String, String, String)](tag, "SUPPLIERS") {
+//    def id = column[Int]("SUP_ID", O.PrimaryKey) // This is the primary key column
+//    def name = column[String]("SUP_NAME")
+//    def street = column[String]("STREET")
+//    def city = column[String]("CITY")
+//    def state = column[String]("STATE")
+//    def zip = column[String]("ZIP")
+//    // Every table needs a * projection with the same type as the table's type parameter
+//    def * = (id, name, street, city, state, zip)
+//  }
+//  val suppliers = TableQuery[Suppliers]
+//
+//  // Definition of the COFFEES table
+//  class Coffees(tag: Tag)
+//      extends Table[String :: Int :: Double :: Int :: Int :: HNil](tag, "COFFEES") {
+//    def name = column[String]("COF_NAME", O.PrimaryKey)
+//    def supID = column[Int]("SUP_ID")
+//    def price = column[Double]("PRICE")
+//    def sales = column[Int]("SALES")
+//    def total = column[Int]("TOTAL")
+//    def * = name :: supID :: price :: sales :: total :: HNil
+//    // A reified foreign key relation that can be navigated to create a join
+//    def supplier = foreignKey("SUP_FK", supID, suppliers)(_.id)
+//  }
+//  val coffees = TableQuery[Coffees]
+//
+//  coffees.map(c => c.name)
+//
+//  case class KvpSlick[C](jsonName: String, slickColumn: Rep[C])
+//
+//  val coffeeSchema =
+//    ("name", string(sv.unique)) ::
+//      ("subID", int(iv.positive)) ::
+//      ("price", double(dv.positive)) ::
+//      ("sales", int(iv.positive)) ::
+//      ("total", int(iv.positive)) ::
+//      KvpNil[String, DefaultValues]
+//
+//  val coffeeSchemaTupled = coffeeSchema.tupled
 
 //  trait InsertH[H <: HList] {}
 //  trait MapRep[H <: HList, HR <: HList, E, C <: TableQuery[E]] {
